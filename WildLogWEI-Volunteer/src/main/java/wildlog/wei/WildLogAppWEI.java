ac07841ead50e0db19e0a2c3f031ee6096b3de39
Henry <36891536+HenryDeLange@users.noreply.github.com>
2019-02-23 09:50:43 +0200

    Initial commit of Admin and Volunteer projects

---
+++ WildLogWEI-Volunteer/src/main/java/wildlog/wei/WildLogAppWEI.java
@@ -0,0 +1,18 @@
+package wildlog.wei;
+
+import org.apache.logging.log4j.Level;
+import wildlog.WildLogApp;
+
+/**
+ * The main class of the application.
+ */
+public class WildLogAppWEI extends WildLogApp {
+    public final static String WILDLOG_WEI_VOLUNTEER_VERSION = "0.0.1";
+    
+    // Stick to the default WildLog application framework
+    public static void main(String[] args) {
+        WildLogApp.main(args);
+        WildLogApp.LOGGER.log(Level.INFO, "WildLogWEI-Volunteer Version: {}", WILDLOG_WEI_VOLUNTEER_VERSION);
+    }
+    
+}
