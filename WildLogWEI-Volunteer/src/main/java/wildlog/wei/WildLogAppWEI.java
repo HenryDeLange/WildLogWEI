f34545d83b5731ac0fb5ed78884ff25b8d25c97b
Henry <36891536+HenryDeLange@users.noreply.github.com>
2019-03-01 19:13:56 +0200

    Nuwe enum gemaak vir die tipe WildLog application.

--- WildLogWEI-Volunteer/src/main/java/wildlog/wei/WildLogAppWEI.java
+++ WildLogWEI-Volunteer/src/main/java/wildlog/wei/WildLogAppWEI.java
@@ -2,6 +2,7 @@ package wildlog.wei;
 
 import org.apache.logging.log4j.Level;
 import wildlog.WildLogApp;
+import wildlog.utils.WildLogApplicationTypes;
 
 /**
  * The main class of the application.
@@ -11,6 +12,7 @@ public class WildLogAppWEI extends WildLogApp {
     
     // Stick to the default WildLog application framework
     public static void main(String[] args) {
+        WILDLOG_TYPE = WildLogApplicationTypes.WILDLOG_WEI_VOLUNTEER;
         WildLogApp.main(args);
         WildLogApp.LOGGER.log(Level.INFO, "WildLogWEI-Volunteer Version: {}", WILDLOG_WEI_VOLUNTEER_VERSION);
     }
