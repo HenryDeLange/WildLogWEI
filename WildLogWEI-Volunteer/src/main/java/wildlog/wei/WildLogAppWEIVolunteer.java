58670c1067a7f37100c229476ad0ca8e2df81e2c
Henry <36891536+HenryDeLange@users.noreply.github.com>
2019-05-06 18:30:45 +0200

    Preparations for first release.

--- WildLogWEI-Volunteer/src/main/java/wildlog/wei/WildLogAppWEIVolunteer.java
+++ WildLogWEI-Volunteer/src/main/java/wildlog/wei/WildLogAppWEIVolunteer.java
@@ -9,13 +9,13 @@ import wildlog.utils.WildLogApplicationTypes;
  * The main class of the application.
  */
 public class WildLogAppWEIVolunteer extends WildLogApp {
-    public final static String WILDLOG_WEI_VOLUNTEER_VERSION = "0.1";
+    public final static String WILDLOG_WEI_VOLUNTEER_VERSION = "1.0.0";
     
     public static void main(String[] args) {
         WILDLOG_APPLICATION_TYPE = WildLogApplicationTypes.WILDLOG_WEI_VOLUNTEER;
         WILDLOG_USER_NAME = "WildLogVolunteer";
         WILDLOG_USER_TYPE = WildLogUserTypes.VOLUNTEER; // Default to the Volunteer user
-        WILDLOG_VERSION = WILDLOG_VERSION + "_" + WILDLOG_WEI_VOLUNTEER_VERSION;
+        WILDLOG_VERSION = WILDLOG_VERSION;
         // Stick to the default WildLog application framework
         WildLogApp.main(args);
         WildLogApp.LOGGER.log(Level.INFO, "WildLogWEI-Volunteer Version: {}", WILDLOG_WEI_VOLUNTEER_VERSION);
