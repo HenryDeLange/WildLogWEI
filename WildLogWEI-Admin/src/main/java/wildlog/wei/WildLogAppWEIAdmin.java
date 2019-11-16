365b967bca1abbdde14e43d90aeb8b8921117ff4
Henry <36891536+HenryDeLange@users.noreply.github.com>
2019-11-16 14:32:28 +0200

    Minor code cleanup. Update version numbers.

--- WildLogWEI-Admin/src/main/java/wildlog/wei/WildLogAppWEIAdmin.java
+++ WildLogWEI-Admin/src/main/java/wildlog/wei/WildLogAppWEIAdmin.java
@@ -9,13 +9,12 @@ import wildlog.utils.WildLogApplicationTypes;
  * The main class of the application.
  */
 public class WildLogAppWEIAdmin extends WildLogApp {
-    public final static String WILDLOG_WEI_ADMIN_VERSION = "1.0.0";
+    public final static String WILDLOG_WEI_ADMIN_VERSION = "1.0.1";
     
     public static void main(String[] args) {
         WILDLOG_APPLICATION_TYPE = WildLogApplicationTypes.WILDLOG_WEI_ADMIN;
         WILDLOG_USER_NAME = "WildLogAdmin";
         WILDLOG_USER_TYPE = WildLogUserTypes.ADMIN; // Default to the Admin user
-        WILDLOG_VERSION = WILDLOG_VERSION;
         // Stick to the default WildLog application framework
         WildLogApp.main(args);
         WildLogApp.LOGGER.log(Level.INFO, "WildLogWEI-Admin Version: {}", WILDLOG_WEI_ADMIN_VERSION);
--- WildLogWEI-Volunteer/src/main/java/wildlog/wei/WildLogAppWEIVolunteer.java
+++ WildLogWEI-Volunteer/src/main/java/wildlog/wei/WildLogAppWEIVolunteer.java
@@ -9,13 +9,12 @@ import wildlog.utils.WildLogApplicationTypes;
  * The main class of the application.
  */
 public class WildLogAppWEIVolunteer extends WildLogApp {
-    public final static String WILDLOG_WEI_VOLUNTEER_VERSION = "1.0.0";
+    public final static String WILDLOG_WEI_VOLUNTEER_VERSION = "1.0.1";
     
     public static void main(String[] args) {
         WILDLOG_APPLICATION_TYPE = WildLogApplicationTypes.WILDLOG_WEI_VOLUNTEER;
         WILDLOG_USER_NAME = "WildLogVolunteer";
         WILDLOG_USER_TYPE = WildLogUserTypes.VOLUNTEER; // Default to the Volunteer user
-        WILDLOG_VERSION = WILDLOG_VERSION;
         // Stick to the default WildLog application framework
         WildLogApp.main(args);
         WildLogApp.LOGGER.log(Level.INFO, "WildLogWEI-Volunteer Version: {}", WILDLOG_WEI_VOLUNTEER_VERSION);
